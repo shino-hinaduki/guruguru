64c31104342954a66b90ef92ba529dd797e20c9b
shino <shino.hinaduki@gmail.com>
2021-11-07 19:04:28 +0900

    (WIP) create ImageGen.vue

---
+++ components/ImageGen.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <el-menu mode="horizontal">
+      <el-menu-item index="1">shino-hinaduki.github.io/guruguru</el-menu-item>
+      <el-menu-item index="2" disabled>Import</el-menu-item>
+      <el-menu-item index="3" disabled>Export</el-menu-item>
+      <el-menu-item index="4" disabled>Info</el-menu-item>
+    </el-menu>
+
+    <el-row :gutter="10" align="middle">
+      <el-col>
+        <el-table :data="tableData" style="width: 100%">
+          <el-table-column prop="name" label="Name" width="120">
+          </el-table-column>
+          <el-table-column fixed="right" label="Operations" width="120">
+            <template slot-scope="scope">
+              <el-button @click="handleClick" type="text" size="small"
+                >Add</el-button
+              >
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+      console.log("click");
+    },
+  },
+  data() {
+    return {
+      tableData: [
+        {
+          name: "Anemoboxer Vanguard",
+          tag: "Fatui",
+        },
+      ],
+    };
+  },
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Tutorial/>
+  <ImageGen/>
 </template>
 
 <script lang="ts">

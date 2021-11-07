8382b57eb9f874e6fce4ab373c48a8e2ec1985c6
shino <shino.hinaduki@gmail.com>
2021-11-07 22:59:40 +0900

    tableに画像表示できた

--- components/ImageGen.vue
+++ components/ImageGen.vue
@@ -4,20 +4,27 @@
       <el-menu-item index="1">shino-hinaduki.github.io/guruguru</el-menu-item>
       <el-menu-item index="2" disabled>Import</el-menu-item>
       <el-menu-item index="3" disabled>Export</el-menu-item>
-      <el-menu-item index="4" disabled>Info</el-menu-item>
+      <el-menu-item index="4" disabled>Clear</el-menu-item>
+      <el-menu-item index="5" disabled>Info</el-menu-item>
     </el-menu>
 
     <el-row :gutter="10" align="middle">
       <el-col>
         <el-table :data="tableData" style="width: 100%">
-          <el-table-column prop="name" label="Name" width="120">
-          </el-table-column>
-          <el-table-column fixed="right" label="Operations" width="120">
-            <template slot-scope="scope">
-              <el-button @click="handleClick" type="text" size="small"
-                >Add</el-button
-              >
+          <el-table-column fixed="left" label="Icon" width="100px">
+            <template slot-scope="props">
+              <el-image
+                style="width: 90px; height: 90px"
+                :src="props.row.url"
+                fit="contain"
+                lazy
+              ></el-image>
             </template>
+          </el-table-column>
+          <el-table-column prop="name" label="Name"> </el-table-column>
+          <el-table-column prop="tag" label="Tag"> </el-table-column>
+          <el-table-column fixed="right" label="Operations" width="120">
+            <el-button type="text">Add</el-button>
           </el-table-column>
         </el-table>
       </el-col>
@@ -27,17 +34,14 @@
 
 <script>
 export default {
-  methods: {
-    handleClick() {
-      console.log("click");
-    },
-  },
+  methods: {},
   data() {
     return {
       tableData: [
         {
           name: "Anemoboxer Vanguard",
           tag: "Fatui",
+          url: "https://static.wikia.nocookie.net/gensin-impact/images/9/96/Enemy_Fatui_Anemoboxer_Vanguard_Icon.png",
         },
       ],
     };

259e26f48bd590baf36ca917ff80e2307cdb9d21
shino <shino.hinaduki@gmail.com>
2021-11-08 01:05:24 +0900

    元素アイコン表示を実装

---
+++ components/ElementIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <el-image :src="imgUrls[element]" fit="contain" lazy></el-image>
+</template>
+
+<script>
+import { default as elementData } from "@/static/json/element-data.json";
+console.log(elementData.data);
+
+export default {
+  data() {
+    return {
+      imgUrls: elementData.data,
+    };
+  },
+  props: {
+    element: {
+      type: String,
+      default: "",
+      required: true,
+    },
+  },
+};
+</script>
--- components/ImageGen.vue
+++ components/ImageGen.vue
@@ -16,16 +16,16 @@
               (data) =>
                 !search ||
                 data.name.toLowerCase().includes(search.toLowerCase()) ||
-                data.tag.toLowerCase().includes(search.toLowerCase())
+                data.category.toLowerCase().includes(search.toLowerCase())
             )
           "
           :default-sort="{ prop: 'name', order: 'descending' }"
           style="width: 100%"
         >
-          <el-table-column fixed="left" label="Icon" width="100px">
+          <el-table-column fixed="left" width="50">
             <template slot-scope="props">
               <el-image
-                style="width: 90px; height: 90px"
+                style="width: 40px; height: 40px"
                 :src="props.row.url"
                 fit="contain"
                 lazy
@@ -33,7 +33,7 @@
             </template>
           </el-table-column>
           <el-table-column prop="name" sortable label="Name">
-            <template slot="header" slot-scope="_">
+            <template slot="header" slot-scope="props">
               <el-input
                 v-model="search"
                 size="mini"
@@ -42,7 +42,13 @@
               />
             </template>
           </el-table-column>
-          <el-table-column prop="tag" sortable label="Tag"> </el-table-column>
+          <el-table-column prop="category" sortable label="Category">
+          </el-table-column>
+          <el-table-column prop="element" sortable width="50">
+            <template slot-scope="props">
+              <ElementIcon :element="props.row.element" />
+            </template>
+          </el-table-column>
           <el-table-column fixed="right" label="Operations" width="120">
             <el-button type="primary" icon="el-icon-plus">Add</el-button>
           </el-table-column>
@@ -53,7 +59,7 @@
 </template>
 
 <script>
-import * as enemyData from "@/static/json/enemy-data.json";
+import { default as enemyData } from "@/static/json/enemy-data.json";
 
 export default {
   methods: {},
